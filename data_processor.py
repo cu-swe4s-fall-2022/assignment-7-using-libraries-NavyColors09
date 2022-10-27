f717c643883a5ce57886106c849b87441ae369b8
Danny Sit <dasi7428@colorado.edu>
2022-10-27 04:59:36 +0000

    changes

--- data_processor.py
+++ data_processor.py
@@ -1,10 +1,28 @@
-# remember to import your libraries!
+import numpy as np 
+import pandas as pd 
 
-def get_random_matrix(num_rows, num_columns):
-	return None
+#This file contains a class that has functions used in the main driver 
+class function: 
+    def get_random_matrix(num_rows, num_columns):
+        while True:
+            try:
+                num_rows = int(num_rows)
+                num_columns = int(num_columns)
+                
+                if num_rows > 0 and num_columns > 0: 
+                    matrix = np.random.rand(num_rows,num_columns) 
+                    return matrix 
+            except ValueError as e:
+                print (e)
 
-def get_file_dimensions(file_name):
-	return (0,0)
+        return matrix 
 
-def write_matrix_to_file(num_rows, num_columns, file_name):
-	return None
+    def get_file_dimensions(file_name):
+        data_frame = pd.read_csv(file_name,header=None) 
+        return data_frame.shape 
+
+    def write_matrix_to_file(num_rows, num_columns, file_name):
+        matrix = get_random_matrix(num_rows,num_columns) 
+        pd.DataFrame(matrix).to_csv(file_name+".csv")
+        #data_frame.to_csv(file_name+'.csv', sep=',')
+        return None 
--- plotter.py
+++ plotter.py
@@ -1,1 +1,41 @@
-# Your code to create majestic plots goes in here!
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+iris = pd.read_csv('iris.data',header=None) 
+iris.columns = ['sepal_length','sepal_width', 'petal_width', 'petal_length','species']
+names = ['sepal_length','sepal_width', 'petal_width', 'petal_length'] 
+plt.boxplot(iris[names],labels=names)
+plt.ylabel('cm')
+plt.savefig("iris_boxplot.png") 
+plt.show() 
+
+for species_name in set(iris['species']):
+    iris_subset = iris[iris['species'] == species_name] #Create a new dataframe
+    plt.scatter(iris_subset['sepal_width'],iris_subset['sepal_length'],label=species_name,s=12)
+
+plt.legend(loc='upper right') 
+plt.xlabel('sepal_length (cm)')
+plt.ylabel('sepal_width (cm)') 
+plt.savefig("petal_width_v_length_scatter.png") 
+plt.show() 
+
+fig,axes = plt.subplots(1,2)
+fig.set_size_inches(12,5)
+
+axes[0].boxplot(iris[names],labels=names)
+axes[0].spines['top'].set_visible(False)
+axes[0].spines['right'].set_visible(False) 
+axes[0].set_ylabel('cm')
+
+for species_name in set(iris['species']):
+    iris_subset = iris[iris['species'] == species_name] #Create a new dataframe
+    axes[1].scatter(iris_subset['sepal_width'],iris_subset['sepal_length'],label=species_name,s=12)
+    
+axes[1].legend(loc='upper right')
+axes[1].spines['top'].set_visible(False)
+axes[1].spines['right'].set_visible(False) 
+axes[1].set_xlabel('sepal_length (cm)')
+axes[1].set_ylabel('sepal_width (cm)') 
+plt.savefig("multi_panel_figure.png") 
+#plt.tight_layout()
+plt.show() 
